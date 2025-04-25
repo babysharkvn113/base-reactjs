5c2324a800e9a8daf90d3ac36bee661a5b04e175
misssa159753 <misssa159753@gmail.com>
2025-04-25 22:18:06 +0000

    add vercel routes

---
+++ src/pages/users/add.page.tsx
@@ -0,0 +1,14 @@
+const AddUserPage = () => {
+    return (
+        <form className="grid grid-cols-12 gap-6">
+            <div className="col-span-8"> 
+                a
+            </div>
+            <div className="col-span-4">
+                b
+            </div>
+        </form>
+    );
+}
+
+export default AddUserPage;
